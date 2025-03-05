6ee0ee5277771a0fdc44f15763fc619e71b156f8
rosen ritter <ch7cly@gmail.com>
2025-03-05 10:59:04 +0300

    fix chromadb interface

--- fastapi/entrypoint.sh
+++ fastapi/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Set the PYTHONPATH environment variable
-# export PYTHONPATH=$PWD:$PATH
+export PYTHONPATH=$PWD:$PATH
 
 # Run the Uvicorn server
-# exec uvicorn face:app --host 0.0.0.0 --port 8001 --log-level debug
+exec uvicorn face:app --host 0.0.0.0 --port 8001 --log-level debug
 
 # Infinite loop
-while true; do
-    # Place your commands here
-    echo "Running..."
+# while true; do
+#     # Place your commands here
+#     echo "Running..."
 
-    # Sleep for a short duration to prevent excessive CPU usage
-    sleep 1
-done
+#     # Sleep for a short duration to prevent excessive CPU usage
+#     sleep 1
+# done
